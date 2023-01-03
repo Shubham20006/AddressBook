35b259f9b5b4e10b9e88b3d127f94612f821de56
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-03 21:55:25 +0530

    Editing Existing contact

--- AddressBook1.cs
+++ AddressBook1.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,7 @@ namespace AddressBookProgram
         public AddressBook()
         {
             AddressBookList = new List<Person>();
+            
         }
         
         public void CreateUser()
@@ -48,7 +50,8 @@ namespace AddressBookProgram
 
                 Person person = new Person(FirstName, LastName, Address, City, State, Zip, PhoneNum, Email);
                 AddPersonToList(person);
-            
+               
+
         }
         private void AddPersonToList(Person person) => AddressBookList.Add(person);
         public void ShowAllPersonsInList()
@@ -60,5 +63,104 @@ namespace AddressBookProgram
             }
             Console.ReadLine();
         }
+
+        public void EditUserInformation()
+        {
+            Console.WriteLine("Which information do you want to Edit?");
+            Console.WriteLine("#1: Firstname, #2: Lastname, 3#: Address, 4#: City, 5#: State, 6#: Zip, 7#: PhoneNum, 8#: Email");
+            var userOption = Console.ReadLine();
+
+            Console.WriteLine("Enter firstname of existing user to be updated");
+            var oldFirstName = Console.ReadLine();
+            EditUserFunction(userOption, oldFirstName);
+        }
+
+        private void EditUserFunction(string userOption, string oldFirstName)
+        {
+            var personsWithMatchingFirstName = AddressBookList.Where(person => person.FirstName == oldFirstName);
+            string newValue;
+
+            if (userOption == "1")
+            {
+                Console.WriteLine("Enter a new first Name");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.FirstName = newValue;                   
+                }
+            }
+            else if (userOption == "2")
+            {
+                Console.WriteLine("Enter a new last name");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.LastName = newValue;               
+                }
+            }
+            else if (userOption == "3")
+            {
+                Console.WriteLine("Enter a new address");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.Address = newValue;
+                }
+            }
+            else if(userOption == "4")
+            {
+                Console.WriteLine("Enter a new City Name");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.City = newValue;
+                }
+            }
+            else if(userOption == "5")
+            {
+                Console.WriteLine("Enter a new State Name");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.State = newValue;
+                }
+            }
+            else if(userOption == "6")
+            {
+                Console.WriteLine("Enter a new Zip code");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.Zip = newValue;
+                }
+            }
+            else if(userOption == "7")
+            {
+                Console.WriteLine("Enter a new PhoneNum");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.PhoneNum = newValue;
+                }
+            }
+            else if(userOption == "8")
+            {
+                Console.WriteLine("Enter a new Email");
+                newValue = Console.ReadLine();
+
+                foreach (var person in personsWithMatchingFirstName)
+                {
+                    person.Email = newValue;
+                }
+            }
+        }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -6,14 +6,23 @@ namespace AddressBookProgram
     public class AddressBook1
     {
         public static void Main(string[] args)
-        {            
-            AddressBook ab = new AddressBook();                     
+        {
+            AddressBook ab = new AddressBook();
             for (int i = 1; i <= 2; i++)
-            { 
-                Console.WriteLine("\n Enter Contact Details {0}",i); 
-                ab.CreateUser(); 
+            {
+                Console.WriteLine("\n Enter Contact Details {0}", i);
+                ab.CreateUser();
             }
             ab.ShowAllPersonsInList();
+            Console.WriteLine("\n Do you want to Edit Information: if yes=1, no=2");
+            int A = Convert.ToInt32(Console.ReadLine());
+            if (A == 1)
+            {
+                ab.EditUserInformation();
+                Console.WriteLine("\n Contact Edited");
+                ab.ShowAllPersonsInList();
+            }
+            
         }
     }
 }
