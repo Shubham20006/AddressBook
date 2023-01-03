1a3c71c033a00bfb0872c857b0fef5ceb1efa49a
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-03 20:44:00 +0530

    Adding new contact in addressBook

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@ namespace AddressBookProgram
     public class AddressBook1
     {
         public static void Main(string[] args)
-        {
-            //   Console.WriteLine("Welcome to the AddressBook Program");
-            AddressBook ab = new AddressBook();
-            ab.CreateUser();
+        {            
+            AddressBook ab = new AddressBook();                     
+            for (int i = 1; i <= 2; i++)
+            { 
+                Console.WriteLine("\n Enter Contact Details {0}",i); 
+                ab.CreateUser(); 
+            }
             ab.ShowAllPersonsInList();
         }
-        
-
     }
 }
